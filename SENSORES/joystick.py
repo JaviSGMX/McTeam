e8fce49c79cf6acbe1ad61b87a1e5f2a03bc65be
JaviSGMX <105335794+JaviSGMX@users.noreply.github.com>
2022-10-18 14:48:52 -0700

    Update joystick.py

--- SENSORES/joystick.py
+++ SENSORES/joystick.py
@@ -1,3 +1,19 @@
 ```python
+from machine import Pin, ADC
+from time import sleep
 
+VRX = ADC(Pin(27))
+VRY = ADC(Pin(26))
+SW = Pin(22,Pin.IN, Pin.PULL_UP)
+
+while True:
+    xAxis = VRX.read_u16()
+    yAxis = VRY.read_u16()
+    switch = SW.value()
+    
+    print("X-axis: " + str(xAxis) + ", Y-axis: " + str(yAxis) + ", Switch " + str(switch))
+    if switch == 0:
+        print("Se presiono el boton!")
+    print(" ")
+    sleep(2)
 ```
