b7b866deea70cc4de91857566cfbdc6081ae1261
JaviSGMX <105335794+JaviSGMX@users.noreply.github.com>
2022-10-18 14:57:50 -0700

    Update KY-018-PHOTOESITOR.py

--- SENSORES/KY-018-PHOTOESITOR.py
+++ SENSORES/KY-018-PHOTOESITOR.py
@@ -14,3 +14,13 @@
   
   ### Mario ivan jimenez fierro 17212145
   ###  Claudio Espinoza Murillo 17211519
+
+  
+  from machine import Pin
+import time
+ 
+ldr = machine.ADC(27)
+ 
+while True:
+     print(ldr.read_u16())
+     time.sleep(0.5)
