8e356a4646f99b38492b9b6ec78c0302445c0bad
McRonald-S <71404620+McRonald-S@users.noreply.github.com>
2022-10-19 14:49:16 -0700

    Update KY-029-MINITWOCOLOR.py

--- SENSORES/KY-029-MINITWOCOLOR.py
+++ SENSORES/KY-029-MINITWOCOLOR.py
@@ -1,3 +1,22 @@
+# INSTITUTO TECNOLOGICO DE TIJUANA 
+  # INGENERIA EN SISTEMAS COMPUTACIONALES
+  
+  ## MATERIA: SISTEMAS PROGRAMABLES
+  ## Equipo de trabajo
+  ## ALUMNOS:
+  
+  ### Sesma Rosales Ronald 20211124
+  
+  ### Rolando Arellano Munguía 18210453
+  
+  ### Perez Rojas Jairo Jaziel 18210515
+  
+  ### Esteban Sánchez Garzón Javier 18210527
+  
+  ### Mario ivan jimenez fierro 17212145
+  
+  ###  Claudio Espinoza Murillo 17211519
+´´´python
 from machine import Pin
 import time
 
@@ -10,3 +29,4 @@ while True: # loop infinitely
         time.sleep(delay_t) # wait
         led.low() # led low
         time.sleep(delay_t) # wait
+´´´
