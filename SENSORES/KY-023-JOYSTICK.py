0fa379fcf2701ca5fbf444e3ccabb2591627129a
McRonald-S <71404620+McRonald-S@users.noreply.github.com>
2022-10-19 14:48:39 -0700

    Update KY-023-JOYSTICK.py

--- SENSORES/KY-023-JOYSTICK.py
+++ SENSORES/KY-023-JOYSTICK.py
@@ -1,3 +1,22 @@
+# INSTITUTO TECNOLOGICO DE TIJUANA 
+  # INGENERIA EN SISTEMAS COMPUTACIONALES
+  
+  ## MATERIA: SISTEMAS PROGRAMABLES
+  ## Equipo de trabajo
+  ## ALUMNOS:
+  
+  ### Sesma Rosales Ronald 20211124
+  
+  ### Rolando Arellano Munguía 18210453
+  
+  ### Perez Rojas Jairo Jaziel 18210515
+  
+  ### Esteban Sánchez Garzón Javier 18210527
+  
+  ### Mario ivan jimenez fierro 17212145
+  
+  ###  Claudio Espinoza Murillo 17211519
+
 ```python
 from machine import Pin, ADC
 from time import sleep
