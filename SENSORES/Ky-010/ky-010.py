20a3f62145c90a28dc753dbb63c2277d39c9aeb7
McRonald-S <71404620+McRonald-S@users.noreply.github.com>
2022-10-24 00:24:02 -0700

    Create ky-010.py

---
+++ SENSORES/Ky-010/ky-010.py
@@ -0,0 +1,31 @@
+# La Libreria RPi No esta en funcionamiento, tienes que cambiarla por la libreria machine
+
+import machine
+import time
+  
+machine.setmode(machine.BCM)
+  
+# El pin de entrada que está conectado con el sensor
+
+machine_PIN = 21
+machine.setup(machine_PIN, GPIO.IN, pull_up_down = machine.PUD_DOWN)
+  
+print "KY-010 Sensor Test [press ctrl+c to end the test]"
+  
+def outputFunction(null):
+        print("Sensor is blocked")
+  
+# detección de señal (flanco ascendente).
+
+machine.add_event_detect(machine_PIN, GPIO.RISING, callback=outputFunction, bouncetime=100) 
+  
+# Bucle del programa principal
+
+try:
+        while True:
+                time.sleep(1)
+  
+# Trabajo de recolección de residuos una vez finalizado el programa
+
+except KeyboardInterrupt:
+        machine.cleanup()
