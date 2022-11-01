a3a8536b86b2a02357456e411c08529fb13bd62d
McRonald-S <71404620+McRonald-S@users.noreply.github.com>
2022-11-01 14:40:42 -0700

    Update ky-004.py

--- SENSORES/KY-004 Button/ky-004.py
+++ SENSORES/KY-004 Button/ky-004.py
@@ -1,10 +1,37 @@
-from machine import Pin
-import time
+# INSTITUTO TECNOLOGICO DE TIJUANA 
+  # INGENERIA EN SISTEMAS COMPUTACIONALES
+  
+  ## MATERIA: SISTEMAS PROGRAMABLES
+  ## Equipo de trabajo
+  ## ALUMNOS:
+  
+  ### Sesma Rosales Ronald 20211124
+  
+  ### Rolando Arellano Munguía 18210453
+  
+  ### Perez Rojas Jairo Jaziel 18210515
+  
+  ### Esteban Sánchez Garzón Javier 18210527
+  
+  ### Mario ivan jimenez fierro 17212145
+  
+  ###  Claudio Espinoza Murillo 17211519
 
-button=Pin(1, Pin.IN)
-led=Pin(12, Pin.OUT)
+```python
+
+from machine import Pin
+from time import sleep
+
+led = Pin(14, Pin.OUT)    # 14 number in is Output
+push_button = Pin(13, Pin.IN)  # 13 number pin is input
 
 while True:
-    state=button.value()
-    led.value(state)
-    time.sleep(0.1)
+  
+  logic_state = push_button.value()
+  if logic_state == True:     # if push_button pressed
+      led.value(1)             # led will turn ON
+  else:                       # if push_button not pressed
+      led.value(0)             # led will turn OFF
+      
+
+```
