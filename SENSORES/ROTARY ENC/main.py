1fd3533ed6273acaa230167b01c27d021a0e649e
McRonald-S <71404620+McRonald-S@users.noreply.github.com>
2022-11-01 21:59:30 -0700

    Update main.py

--- SENSORES/ROTARY ENC/main.py
+++ SENSORES/ROTARY ENC/main.py
@@ -1,3 +1,22 @@
+# INSTITUTO TECNOLOGICO DE TIJUANA 
+  # INGENERIA EN SISTEMAS COMPUTACIONALES
+  
+  ## MATERIA: SISTEMAS PROGRAMABLES
+  ## Equipo de trabajo
+  ## ALUMNOS:
+  ### Sesma Rosales Ronald 20211124
+  
+  ### Rolando Arellano Munguía 18210453
+  
+  ### Perez Rojas Jairo Jaziel 18210515
+  
+  ### Esteban Sánchez Garzón Javier 18210527
+  
+  ### Mario ivan jimenez fierro 17212145
+  ###  Claudio Espinoza Murillo 17211519
+
+```python
+
 from machine import Pin
 from time import sleep
 
@@ -24,3 +43,4 @@ while True:
          switch_presionado = True
      if SW.value() == True and switch_presionado:
          switch_presionado = False
+´´´
