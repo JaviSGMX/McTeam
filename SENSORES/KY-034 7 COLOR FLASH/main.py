5542081ea051d289dbc1abb466292d5cb570113e
JaviSGMX <105335794+JaviSGMX@users.noreply.github.com>
2022-11-16 14:45:51 -0800

    Update main.py

--- SENSORES/KY-034 7 COLOR FLASH/main.py
+++ SENSORES/KY-034 7 COLOR FLASH/main.py
@@ -18,3 +18,12 @@
   ###  Claudio Espinoza Murillo 17211519
   
   ´´´python
+from machine import Pin
+import time
+
+led_pins = [16] # pins where RGB LED is wired
+leds = [Pin(led_pins[0],Pin.OUT)] # pin control array
+delay_t = 0.1 # seconds to delay between toggles
+while True: # loop infinitely
+    for led in leds: # loop through each led
+        led.high() # led high
