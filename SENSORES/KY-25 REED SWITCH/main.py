a6f9af6e949d21d994a5f81891892c44bdf95b00
McRonald-S <71404620+McRonald-S@users.noreply.github.com>
2022-11-16 14:41:53 -0800

    Update main.py

--- SENSORES/KY-25 REED SWITCH/main.py
+++ SENSORES/KY-25 REED SWITCH/main.py
@@ -18,5 +18,18 @@
   ###  Claudio Espinoza Murillo 17211519
   
   ´´´python
-  
+from machine import Pin
+import time
+
+ReedSensor = Pin(18, Pin.IN)
+while True:
+    value = ReedSensor.value()
+    print(value, end = " ")
+    if value == 0:
+        print("Sin Campo Magnetico")
+    else:
+        print("Campo Magnetico")
+    time.sleep(0.5)
+    
+    ´´´
   
