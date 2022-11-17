e68e2670c40b2567f16575de35e517efabbf8a01
McRonald-S <71404620+McRonald-S@users.noreply.github.com>
2022-11-17 01:01:05 -0800

    Update ky-010.py

--- SENSORES/Ky-010/ky-010.py
+++ SENSORES/Ky-010/ky-010.py
@@ -1,3 +1,24 @@
+# INSTITUTO TECNOLOGICO DE TIJUANA 
+# INGENERIA EN SISTEMAS COMPUTACIONALES
+
+## MATERIA: SISTEMAS PROGRAMABLES
+## Equipo de trabajo
+## ALUMNOS:
+
+### Sesma Rosales Ronald 20211124
+
+### Rolando Arellano Munguía 18210453
+
+### Perez Rojas Jairo Jaziel 18210515
+
+### Esteban Sánchez Garzón Javier 18210527
+
+### Mario ivan jimenez fierro 17212145
+
+###  Claudio Espinoza Murillo 17211519
+
+
+
 # La Libreria RPi No esta en funcionamiento, tienes que cambiarla por la libreria machine
 
 import machine
@@ -29,3 +50,5 @@ try:
 
 except KeyboardInterrupt:
         machine.cleanup()
+
+    ´´´
