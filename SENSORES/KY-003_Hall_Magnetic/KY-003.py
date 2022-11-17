4fc4a10a78c3b3e2096eab1878b99d095b02f9d1
McRonald-S <71404620+McRonald-S@users.noreply.github.com>
2022-11-17 00:56:51 -0800

    Update KY-003.py

--- SENSORES/KY-003_Hall_Magnetic/KY-003.py
+++ SENSORES/KY-003_Hall_Magnetic/KY-003.py
@@ -1,4 +1,25 @@
-from machine import Pin
+# INSTITUTO TECNOLOGICO DE TIJUANA 
+# INGENERIA EN SISTEMAS COMPUTACIONALES
+
+## MATERIA: SISTEMAS PROGRAMABLES
+## Equipo de trabajo
+## ALUMNOS:
+
+### Sesma Rosales Ronald 20211124
+
+### Rolando Arellano Munguía 18210453
+
+### Perez Rojas Jairo Jaziel 18210515
+
+### Esteban Sánchez Garzón Javier 18210527
+
+### Mario ivan jimenez fierro 17212145
+
+###  Claudio Espinoza Murillo 17211519
+  
+  ´´´python
+    
+    from machine import Pin
 import utime
 
 sensor=Pin(26, Pin.IN)
@@ -12,3 +33,6 @@ while True:
         print("Campo magnetico detectado")
         utime.sleep(1)
 utime.sleep(1)
+    
+    
+´´´
