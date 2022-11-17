ac8d9d0474e1939abb92e47569ba687f03cc5cc7
McRonald-S <71404620+McRonald-S@users.noreply.github.com>
2022-11-17 00:51:39 -0800

    Update Flame.py

--- SENSORES/Flame/Flame.py
+++ SENSORES/Flame/Flame.py
@@ -1,3 +1,23 @@
+# INSTITUTO TECNOLOGICO DE TIJUANA 
+  # INGENERIA EN SISTEMAS COMPUTACIONALES
+  
+  ## MATERIA: SISTEMAS PROGRAMABLES
+  ## Equipo de trabajo
+  ## ALUMNOS:
+  
+  ### Sesma Rosales Ronald 20211124
+  
+  ### Rolando Arellano Munguía 18210453
+  
+  ### Perez Rojas Jairo Jaziel 18210515
+  
+  ### Esteban Sánchez Garzón Javier 18210527
+  
+  ### Mario ivan jimenez fierro 17212145
+  
+  ###  Claudio Espinoza Murillo 17211519
+  
+  ´´´python
 from machine import Pin
 import utime
 
@@ -12,3 +32,4 @@ while True:
        print("No Flame")
        utime.sleep(1)
 utime.sleep(0.1)
+´´´
