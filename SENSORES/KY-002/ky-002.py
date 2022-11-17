bd1763fb561c88f18c0c19fb37993b7f97fe82ba
McRonald-S <71404620+McRonald-S@users.noreply.github.com>
2022-11-17 00:55:40 -0800

    Update ky-002.py

--- SENSORES/KY-002/ky-002.py
+++ SENSORES/KY-002/ky-002.py
@@ -1,18 +1,25 @@
-#Ojo a este codigo
-
-import sys
-# import RPi.GPIO as GPIO
-from machine import Pin
-
-
-estadoKY = 0
-KY002 = Pin(7, Pin.IN) # Pin asignado para Sensor Vibración
-LED = Pin(11, Pin.OUT) # Pin asignado para LED
-
-while True:
-    estadoKY = KY002.value() # Busca el valor del sensor KY002 booleano (si detecta movimiento)
-    if estadoKY == 1: # Si detecta movimiento 
-        if LED.value == 0: # Si el LED está apagado
-            LED.value(1) #Se enciende
-        else:  # Si no
-            LED.value(0) #Se apaga
+# INSTITUTO TECNOLOGICO DE TIJUANA 
+  # INGENERIA EN SISTEMAS COMPUTACIONALES
+  
+  ## MATERIA: SISTEMAS PROGRAMABLES
+  ## Equipo de trabajo
+  ## ALUMNOS:
+  
+  ### Sesma Rosales Ronald 20211124
+  
+  ### Rolando Arellano Munguía 18210453
+  
+  ### Perez Rojas Jairo Jaziel 18210515
+  
+  ### Esteban Sánchez Garzón Javier 18210527
+  
+  ### Mario ivan jimenez fierro 17212145
+  
+  ###  Claudio Espinoza Murillo 17211519
+  
+  ´´´python
+    
+    
+    
+    
+´´´
